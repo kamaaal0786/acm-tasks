941c945d8f19a672aff8324f4ed113c10cb05bb0
kamaaal0786 <kamaalmagdoomaf2006@gmail.com>
2025-08-19 00:04:37 +0530

    task 1
    acm task 1

---
+++ task 1/app.js
@@ -0,0 +1,4 @@
+function changeColor() {
+  const heading = document.getElementById("heading");
+  heading.style.color = "red";
+}
---
+++ task 1/newFile.js
@@ -0,0 +1,8 @@
+<script>
+    function changeColor() {
+        // get heading element
+        let} heading = document.getElementById("heading");
+    // change text color
+    heading.style.color = "red";
+    }
+</script>;
