9183d512655e7a2f186f22e2180e83fd2fa98453
kamaaal0786 <kamaalmagdoomaf2006@gmail.com>
2025-08-19 00:08:22 +0530

    Delete newFile.js

--- task 1/newFile.js
+++
@@ -1,8 +0,0 @@
-<script>
-    function changeColor() {
-        // get heading element
-        let} heading = document.getElementById("heading");
-    // change text color
-    heading.style.color = "red";
-    }
-</script>;
